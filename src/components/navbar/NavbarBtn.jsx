173b8636c0cd89c1ed6bb687b6423280de604d52
Hassanjaved17 <hassanaptech9@gmail.com>
2025-08-18 00:58:23 +0500

    update navbar-btn

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -4,18 +4,19 @@ import { FiDownload } from "react-icons/fi";
 const NavbarBtn = () => {
   return (
     <a
-      href="/resume.pdf"
+      href="/resume/HassanResume.pdf"
       download="HassanResume.pdf"
-      target="_blank" 
+      target="_blank"
       rel="noopener noreferrer"
       className="px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow"
     >
       Resume
       <div className="sm:hidden md:block">
-      <FiDownload />
+        <FiDownload />
 
       </div>
     </a>
+    
   );
 };
 
