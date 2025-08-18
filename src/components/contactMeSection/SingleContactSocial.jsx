dd37c896c2972a3c20eaac53f0ee01033194e903
Hassanjaved17 <hassanaptech9@gmail.com>
2025-08-18 15:10:50 +0500

    update

--- src/components/contactMeSection/SingleContactSocial.jsx
+++ src/components/contactMeSection/SingleContactSocial.jsx
@@ -1,10 +1,14 @@
-const SingleContactSocial = ({ Icon, link }) => {
+const SingleContactSocial = ({ link, Icon, hoverColor  }) => {
   return (
-    <div className="text-2xl h-12 w-12 border border-orange text-orange rounded-full p-3 flex items-center justify-center ">
-      <a href={link} className="cursor-pointer">
-        <Icon />
-      </a>
-    </div>
+    <a 
+      href={link} 
+      target="_blank" 
+      rel="noopener noreferrer"
+      className={`p-3 border-2 border-[#FB9718] rounded-full text-2xl
+                 transition-all duration-300 hover:bg-[#FB9718] hover:text-white ${hoverColor}`}
+    >
+      <Icon />
+    </a>
   );
 };
 
