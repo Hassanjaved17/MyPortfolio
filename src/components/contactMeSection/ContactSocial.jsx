632240724d9da5c3e39f8286454dc402be542beb
Hassanjaved17 <hassanaptech9@gmail.com>
2025-08-21 23:41:27 +0500

    add favicon

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -8,21 +8,21 @@ import { FaWhatsapp } from "react-icons/fa";
 const ContactSocial = () => {
   return (
     <div className="flex gap-4 text-[#FB9718] ">
-      <SingleContactSocial 
-        link="https://www.linkedin.com/in/hassan-javed-69766b336/" 
-        Icon={FaLinkedinIn} 
+      <SingleContactSocial
+        link="https://www.linkedin.com/in/hassan-javed-69766b336/"
+        Icon={FaLinkedinIn}
       />
-      <SingleContactSocial 
-        link="https://github.com/Hassanjaved17" 
-        Icon={FiGithub} 
+      <SingleContactSocial
+        link="https://github.com/Hassanjaved17"
+        Icon={FiGithub}
       />
-      <SingleContactSocial 
-        link="https://www.instagram.com/hassan_javed_17/" 
-        Icon={FaInstagram} 
+      <SingleContactSocial
+        link="https://www.instagram.com/hassan_javed_17/"
+        Icon={FaInstagram}
       />
-      <SingleContactSocial 
-        link="https://wa.me/923302479878"  
-        Icon={FaWhatsapp} 
+      <SingleContactSocial
+        link="https://wa.me/923302479878"
+        Icon={FaWhatsapp}
       />
     </div>
   );
--- src/components/contactMeSection/SingleContactSocial.jsx
+++ src/components/contactMeSection/SingleContactSocial.jsx
@@ -1,8 +1,8 @@
-const SingleContactSocial = ({ link, Icon, hoverColor  }) => {
+const SingleContactSocial = ({ link, Icon, hoverColor }) => {
   return (
-    <a 
-      href={link} 
-      target="_blank" 
+    <a
+      href={link}
+      target="_blank"
       rel="noopener noreferrer"
       className={`p-3 border-2 border-[#FB9718] rounded-full text-2xl
                  transition-all duration-300 hover:bg-[#FB9718] hover:text-white ${hoverColor}`}
